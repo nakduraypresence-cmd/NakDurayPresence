2a934412e827a3f89fb805997ffb58f466620eca
Vinii <mvvinicius231017vk@gmail.com>
2026-08-16 20:20:49 -0300

    Corrige porta para o Render

--- app.py
+++ app.py
@@ -186,3 +186,10 @@ def treino_detalhe(id):
 if __name__ == "__main__":
     init_db()
     app.run(debug=True)
+
+    import os
+
+if __name__ == "__main__":
+    init_db()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
